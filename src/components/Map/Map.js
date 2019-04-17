2f1019b1e2bcf17e807c85de208e4218c31a616a
Oscar Paesi <oscar.paesi@gmail.com>
2019-04-17 11:29:48 -0300

    feat: added predefined locations to the map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,12 +1,13 @@
 import './Map.css';
 import React, { Component } from 'react';
 import { compose, withProps } from 'recompose';
-import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
+import { locations } from '../../data/locations';
 
 const MapWrapper = compose(
   withProps({
     loadingElement: (
-      <span>There was an error while loading the Google Maps scripts. Please try again later.</span>
+      <span>It was not possible to load Google Maps.</span>
     ),
     containerElement: (
       <main className="map-container" role="application" tabIndex="0" />
@@ -19,17 +20,32 @@ const MapWrapper = compose(
   withGoogleMap
 )(props =>
   <GoogleMap
-    defaultZoom={ 13 }
-    defaultCenter={ {lat: -30.034647, lng: -51.217658} } // Center of Porto Alegre
-  />
+    defaultZoom={ 16 }
+    defaultCenter={ {lat: -30.02502, lng: -51.191165} } // Center of the neighborhood
+  >
+    {
+      props.locations.map(location =>
+        <Marker key={ location.id } position={ location.position } />
+      )
+    }
+  </GoogleMap>
 );
 
 class Map extends Component {
 
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { locations };
+  }
+
+  render = () => {
     const { googleMapURL } = this.props;
     return (
-      <MapWrapper googleMapURL= { googleMapURL } />
+      <MapWrapper
+        googleMapURL={ googleMapURL }
+        locations={ this.state.locations }
+      />
     );
   }
 }
---
+++ src/data/locations.js
@@ -0,0 +1,14 @@
+export const locations = [
+  {id:'l01' ,title: 'Fazenda Barbanegra',         position: {lat: -30.025769, lng: -51.189295}},
+  {id:'l02' ,title: 'Le Bateau Ivre',             position: {lat: -30.028217, lng: -51.187401}},
+  {id:'l03' ,title: 'O Galo Cinza',               position: {lat: -30.024158, lng: -51.188978}},
+  {id:'l04' ,title: 'William & Sons',             position: {lat: -30.029411, lng: -51.189385}},
+  {id:'l05' ,title: 'Charlie Brownie',            position: {lat: -30.026026, lng: -51.193150}},
+  {id:'l06' ,title: 'Sharin',                     position: {lat: -30.022891, lng: -51.192314}},
+  {id:'l07' ,title: 'Ciao Pizzeria Napoletana',   position: {lat: -30.026455, lng: -51.188857}},
+  {id:'l08' ,title: 'Zero Sen',                   position: {lat: -30.026493, lng: -51.188950}},
+  {id:'l09' ,title: 'Brizza Pizzeria Napoletana', position: {lat: -30.022364, lng: -51.194586}},
+  {id:'l10' ,title: 'Fornellone',                 position: {lat: -30.021352, lng: -51.194929}},
+  {id:'l11' ,title: 'Sette PastaBar',             position: {lat: -30.021407, lng: -51.190905}},
+  {id:'l12' ,title: 'Diego Andino Pâtisserie',    position: {lat: -30.030123, lng: -51.188260}}
+];
